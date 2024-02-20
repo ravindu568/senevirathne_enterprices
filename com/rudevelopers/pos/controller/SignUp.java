68eae3b8bdd6f00c87cf4ec7a29fd03683e42a48
Ravindu Wanshanatha <63859006+ravindu568@users.noreply.github.com>
2024-02-20 22:12:15 +0530

    non finalize db connection

--- com/rudevelopers/pos/controller/SignUp.java
+++ com/rudevelopers/pos/controller/SignUp.java
@@ -9,6 +9,9 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SignUp {
     public JFXTextField txtEmail;
@@ -19,7 +22,20 @@ public class SignUp {
         setUi("LoginForm");
     }
 
-    public void btnRegisterNowOnAction(ActionEvent actionEvent) {
+    public void btnRegisterNowOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+
+        try{
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3307/mydb","root","1234");
+            String sql="INSERT INTO user VALUES (?,?)";
+
+        }catch(ClassNotFoundException | SQLException){
+
+        }
+
+
+
     }
 
     public void setUi(String url) throws IOException {
