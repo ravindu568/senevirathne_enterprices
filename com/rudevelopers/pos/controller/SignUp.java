115c68aabb33e83d9799e01493c27ea7ccdf0e36
Ravindu Wanshanatha <63859006+ravindu568@users.noreply.github.com>
2024-02-22 20:07:23 +0530

    db connection stablished

--- com/rudevelopers/pos/controller/SignUp.java
+++ com/rudevelopers/pos/controller/SignUp.java
@@ -5,12 +5,14 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class SignUp {
@@ -29,8 +31,20 @@ public class SignUp {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3307/mydb","root","1234");
             String sql="INSERT INTO user VALUES (?,?)";
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,txtEmail.getText());
+            preparedStatement.setString(2,txtPassword.getText());
 
-        }catch(ClassNotFoundException | SQLException){
+            if(preparedStatement.executeUpdate()>0){
+                new Alert(Alert.AlertType.CONFIRMATION,"User Saved!").show();
+            }else{
+                new Alert(Alert.AlertType.WARNING,"Try Again!").show();
+            }
+
+
+        }catch(ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.WARNING,"Try Again!").show();
 
         }
 
